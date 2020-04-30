febfd930f025b6f4d7d5adcc0e6a98a42ab76f9e
unknown <rafaelbatista.sth@gmail.com>
2020-04-30 14:26:51 -0300

    nodemon configuration

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.send("Teste");
+app.use(express.json());
+
+app.post("/users", (request, response) => {
+  const body = request.body;
+
+  console.log(body);
+
+  return response.json({
+    evento: "Semana OminiStack 11",
+    aluno: "Rafael Batista",
+  });
 });
 app.listen(3333);
