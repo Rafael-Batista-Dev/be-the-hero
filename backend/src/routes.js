39ef66a4bb18b2add863ab20c3d28680decb5469
unknown <rafaelbatista.sth@gmail.com>
2020-04-30 19:09:29 -0300

    create migrates

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,31 @@
 const express = require("express");
+const crypto = require("crypto");
+
+const connection = require("./database/connection");
+
 const routes = express.Router();
 
-routes.post("/users", (request, response) => {
-  const body = request.body;
+routes.get("/ongs", async (request, response) => {
+  const ongs = await connection("ongs").select("*");
 
-  console.log(body);
+  return response.json(ongs);
+});
 
-  return response.json({
-    evento: "Semana OminiStack 11",
-    aluno: "Rafael Batista",
+routes.post("/ongs", async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf,
   });
+
+  return response.json({ id });
 });
 
 module.exports = routes;
