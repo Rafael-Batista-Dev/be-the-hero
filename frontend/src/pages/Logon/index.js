9afbb4c381c235f2e9eb12e912300acf73d34d67
unknown <rafaelbatista.sth@gmail.com>
2020-05-01 23:39:55 -0300

    create logon

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./styles.css";
+import Lottie from "react-lottie";
+
+import { FiLogIn } from "react-icons/fi";
+
+import logoImg from "../../assets/logo.svg";
+import animationData from "../../assets/hero.json";
+
+function logon() {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="be the hero" />
+        <form>
+          <h1>Login</h1>
+          <input type="text" placeholder="Sua Id" />
+          <button className="button" type="submit">
+            Entrar
+          </button>
+
+          <a href="/register">
+            <FiLogIn size={16} color="#8c69c1" /> Não tenho cadastro
+          </a>
+        </form>
+      </section>
+      <div className="lottie-container">
+        <div className="lottie-json" style={{ marginTop: "4rem" }}>
+          <Lottie
+            width={400}
+            height={400}
+            options={{
+              loop: true,
+              autoplay: true,
+              animationData,
+              rendererSettings: {
+                preserveAspectRatio: "xMidYMid slice",
+              },
+            }}
+          ></Lottie>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default logon;
