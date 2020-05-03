3c0c399f838be0072a8974b18beb4afe76bcf59b
unknown <rafaelbatista.sth@gmail.com>
2020-05-03 13:33:23 -0300

    list end delete of icidents by the api

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
 import "./styles.css";
 import logoImg from "../../assets/logo.svg";
-import { Link } from "react-router-dom";
 
 import { FiPower } from "react-icons/fi";
 import { FiTrash2 } from "react-icons/fi";
 
-function profile() {
+import api from "../../services/api";
+
+export default function Profile() {
+  const [incidents, setIncidents] = useState([]);
+
+  const history = useHistory();
+  const ongId = localStorage.getItem("ongId");
+  const ongName = localStorage.getItem("ongName");
+
+  useEffect(() => {
+    api
+      .get("profile", {
+        headers: {
+          Authorization: ongId,
+        },
+      })
+      .then((response) => setIncidents(response.data));
+  }, [ongId]);
+
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId,
+        },
+      });
+
+      setIncidents(incidents.filter((incident) => incident.id !== id));
+    } catch (error) {
+      alert(`Erro ao deletar, tente novamente.`);
+    }
+  }
+
+  function handleLogout() {
+    localStorage.clear();
+
+    history.push("/");
+  }
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="be the hero" />
-        <span>Bem vindo, ao centro de zoonoses cariri</span>
+        <span>Bem-vinda, {ongName}</span>
         <Link className="button" to="/new-incident">
           Cadastrar novo caso
         </Link>
@@ -23,190 +60,33 @@ function profile() {
         <div style={{ marginTop: "7rem" }} className="card">
           <h1>Casos cadastrados</h1>
           <ul>
-            <li>
-              <strong>CASOS:</strong>
-              <p>Caso teste</p>
+            {incidents.map((incident) => (
+              <li key={incident.id}>
+                <strong>CASO:</strong>
+                <p>{incident.title}</p>
 
-              <strong>DESCRIÇÃO:</strong>
-              <p>Descrição do Caso teste</p>
+                <strong>DESCRIÇÃO:</strong>
+                <p>{incident.description}</p>
 
-              <strong>VALOR:</strong>
-              <p>R$ 220,00</p>
-              <button type="button">
-                <FiTrash2 />
-              </button>
-            </li>
+                <strong>VALOR:</strong>
+                <p>
+                  {Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  }).format(incident.value)}
+                </p>
 
-            <li>
-              <strong>CASOS:</strong>
-              <p>Caso teste</p>
-
-              <strong>DESCRIÇÃO:</strong>
-              <p>Descrição do Caso teste</p>
-
-              <strong>VALOR:</strong>
-              <p>R$ 220,00</p>
-              <button type="button">
-                <FiTrash2 />
-              </button>
-            </li>
-
-            <li>
-              <strong>CASOS:</strong>
-              <p>Caso teste</p>
-
-              <strong>DESCRIÇÃO:</strong>
-              <p>Descrição do Caso teste</p>
-
-              <strong>VALOR:</strong>
-              <p>R$ 220,00</p>
-              <button type="button">
-                <FiTrash2 />
-              </button>
-            </li>
-
-            <li>
-              <strong>CASOS:</strong>
-              <p>Caso teste</p>
-
-              <strong>DESCRIÇÃO:</strong>
-              <p>Descrição do Caso teste</p>
-
-              <strong>VALOR:</strong>
-              <p>R$ 220,00</p>
-              <button type="button">
-                <FiTrash2 />
-              </button>
-            </li>
-            <li>
-              <strong>CASOS:</strong>
-              <p>Caso teste</p>
-
-              <strong>DESCRIÇÃO:</strong>
-              <p>Descrição do Caso teste</p>
-
-              <strong>VALOR:</strong>
-              <p>R$ 220,00</p>
-              <button type="button">
-                <FiTrash2 />
-              </button>
-            </li>
-
-            <li>
-              <strong>CASOS:</strong>
-              <p>Caso teste</p>
-
-              <strong>DESCRIÇÃO:</strong>
-              <p>Descrição do Caso teste</p>
-
-              <strong>VALOR:</strong>
-              <p>R$ 220,00</p>
-              <button type="button">
-                <FiTrash2 />
-              </button>
-            </li>
-
-            <li>
-              <strong>CASOS:</strong>
-              <p>Caso teste</p>
-
-              <strong>DESCRIÇÃO:</strong>
-              <p>Descrição do Caso teste</p>
-
-              <strong>VALOR:</strong>
-              <p>R$ 220,00</p>
-              <button type="button">
-                <FiTrash2 />
-              </button>
-            </li>
-
-            <li>
-              <strong>CASOS:</strong>
-              <p>Caso teste</p>
-
-              <strong>DESCRIÇÃO:</strong>
-              <p>Descrição do Caso teste</p>
-
-              <strong>VALOR:</strong>
-              <p>R$ 220,00</p>
-              <button type="button">
-                <FiTrash2 />
-              </button>
-            </li>
-
-            <li>
-              <strong>CASOS:</strong>
-              <p>Caso teste</p>
-
-              <strong>DESCRIÇÃO:</strong>
-              <p>Descrição do Caso teste</p>
-
-              <strong>VALOR:</strong>
-              <p>R$ 220,00</p>
-              <button type="button">
-                <FiTrash2 />
-              </button>
-            </li>
-            <li>
-              <strong>CASOS:</strong>
-              <p>Caso teste</p>
-
-              <strong>DESCRIÇÃO:</strong>
-              <p>Descrição do Caso teste</p>
-
-              <strong>VALOR:</strong>
-              <p>R$ 220,00</p>
-              <button type="button">
-                <FiTrash2 />
-              </button>
-            </li>
-
-            <li>
-              <strong>CASOS:</strong>
-              <p>Caso teste</p>
-
-              <strong>DESCRIÇÃO:</strong>
-              <p>Descrição do Caso teste</p>
-
-              <strong>VALOR:</strong>
-              <p>R$ 220,00</p>
-              <button type="button">
-                <FiTrash2 />
-              </button>
-            </li>
-
-            <li>
-              <strong>CASOS:</strong>
-              <p>Caso teste</p>
-
-              <strong>DESCRIÇÃO:</strong>
-              <p>Descrição do Caso teste</p>
-
-              <strong>VALOR:</strong>
-              <p>R$ 220,00</p>
-              <button type="button">
-                <FiTrash2 />
-              </button>
-            </li>
-
-            <li>
-              <strong>CASOS:</strong>
-              <p>Caso teste</p>
-
-              <strong>DESCRIÇÃO:</strong>
-              <p>Descrição do Caso teste</p>
-
-              <strong>VALOR:</strong>
-              <p>R$ 220,00</p>
-              <button type="button">
-                <FiTrash2 />
-              </button>
-            </li>
+                <button
+                  onClick={() => handleDeleteIncident(incident.id)}
+                  type="button"
+                >
+                  <FiTrash2 />
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
     </div>
   );
 }
-
-export default profile;
