d97a9d87a1d8be8f057816d6b8cb90fcdf4565f3
unknown <rafaelbatista.sth@gmail.com>
2020-05-03 14:03:53 -0300

    conecting page new incident in api

--- frontend/src/pages/Newincident/index.js
+++ frontend/src/pages/Newincident/index.js
@@ -1,12 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./styles.css";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+
+import api from "../../services/api";
+
+import "./styles.css";
+
 import logoImg from "../../assets/logo.svg";
 
-import { FiPower } from "react-icons/fi";
+export default function Incident() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
 
-function incident() {
+  const history = useHistory();
+
+  const ongId = localStorage.getItem("ongId");
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post("incidents", data, {
+        headers: {
+          Authorization: ongId,
+        },
+      });
+
+      history.push("/profile");
+    } catch (error) {
+      alert("Erro ao cadastrar caso, tente novamente.");
+    }
+  }
+
   return (
     <div className="newincident-container">
       <div className="content">
@@ -26,10 +58,22 @@ function incident() {
           </Link>
         </section>
         <div className="form-container">
-          <form>
-            <input placeholder="Título do caso" />
-            <textarea placeholder="Descrição" />
-            <input placeholder="Valor em reais" />
+          <form onSubmit={handleNewIncident}>
+            <input
+              placeholder="Título do caso"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <textarea
+              placeholder="Descrição"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <input
+              placeholder="Valor em reais"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
             <button className="button" type="submit">
               Cadastrar
             </button>
@@ -39,5 +83,3 @@ function incident() {
     </div>
   );
 }
-
-export default incident;
