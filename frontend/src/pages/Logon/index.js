cc79ce398e06beb494cf3b487a4926449c601f9a
unknown <rafaelbatista.sth@gmail.com>
2020-05-03 12:50:27 -0300

    connect login frontend in api to authentication

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "./styles.css";
 import Lottie from "react-lottie";
 
@@ -10,16 +10,38 @@ import { FiLogIn } from "react-icons/fi";
 import logoImg from "../../assets/logo.svg";
 import animationData from "../../assets/hero.json";
 
-export default function logon() {
+export default function Logon() {
   const [id, setId] = useState("");
+
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("sessions", { id });
+      console.log(response.data.name);
+
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", response.data.name);
+
+      history.push("/profile");
+    } catch (error) {
+      alert("Falha na autenticação, tente novamente");
+    }
+  }
 
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="be the hero" />
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Login</h1>
-          <input type="text" placeholder="Sua Id" />
+          <input
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            placeholder="Sua Id"
+          />
           <button className="button" type="submit">
             Entrar
           </button>
