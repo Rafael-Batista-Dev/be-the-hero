1e4ecebbe4c18f62a9c0b54a87d37b8ba6d6f57b
unknown <rafaelbatista.sth@gmail.com>
2020-05-04 18:23:40 -0300

    start

---
+++ mobile/src/routes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const AppStack = createStackNavigator();
+
+import Incidents from "./views/Incidents";
+import Detail from "./views/Detail";
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator>
+        <AppStack.Screen name="Incidents" component={Incidents} />
+        <AppStack.Screen name="Detail" component={Detail} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
