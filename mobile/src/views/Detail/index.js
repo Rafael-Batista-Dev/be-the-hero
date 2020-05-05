857d70395d4237f0e27183fa00d31a53f04a9fe2
unknown <rafaelbatista.sth@gmail.com>
2020-05-05 12:10:53 -0300

    installed and consuming expo-mail-composer

--- mobile/src/views/Detail/index.js
+++ mobile/src/views/Detail/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Feather } from "@expo/vector-icons";
+import * as MailComposer from "expo-mail-composer";
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 
@@ -8,9 +9,19 @@ import styles from "./styles";
 
 export default function Detail() {
   const navigation = useNavigation();
+  const message =
+    "Olá Centro de Zoonoses do Cariri, estuou entrando em contato, para poder contrbuir com o caso da cadilina atropelada";
 
   function navigateBack() {
     navigation.goBack();
+  }
+
+  function sendMail() {
+    MailComposer.composeAsync({
+      subject: "Herói do caso: Cadelinha atropelada",
+      recipients: ["rafaelbatis.sth@gmail.com"],
+      body: message,
+    });
   }
 
   return (
@@ -42,7 +53,7 @@ export default function Detail() {
           <TouchableOpacity style={styles.action} onPress={() => {}}>
             <Text style={styles.actionText}>WhatsApp</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
             <Text style={styles.actionText}>E-mail</Text>
           </TouchableOpacity>
         </View>
